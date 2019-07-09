28502181b48aa1920b0ae508266c9babadf8b046
renxiaoya <renxiaoya@shuidihuzhu.com>
2019-07-09 20:19:29 +0800

    建造者模式v2-定义

---
+++ src/com/redmaple/design/pattern/creational/builder/v2/Game.java
@@ -0,0 +1,11 @@
+package com.redmaple.design.pattern.creational.builder.v2;
+
+public class Game {
+    private String name;
+    private String introduce;
+    private String team;
+    private String video;
+    private String community;
+
+
+}
